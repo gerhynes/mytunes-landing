79db50f05f6ce4509e36015d8f401495f818fbb0
Gerard Hynes <gkhynes@gmail.com>
2018-11-10 20:31:02 +0000

    Add smooth scrolling

--- dist/js/main.js
+++ dist/js/main.js
@@ -22,3 +22,22 @@ $(function() {
   });
   $(".open-menu").height($(window).height());
 });
+
+// Smooth Scrolling
+$(".cf a").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
